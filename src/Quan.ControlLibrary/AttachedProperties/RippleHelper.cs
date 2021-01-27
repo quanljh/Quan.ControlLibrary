ebdb2d45566ef4b9126056049b417f4ae26f675b
quanljh <quanljh@gmail.com>
2021-01-27 18:22:37 +0800

    Prepare to add QuanRippleLine.

---
+++ src/Quan.ControlLibrary/AttachedProperties/RippleHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using System.Windows;
+
+namespace Quan.ControlLibrary
+{
+    public static class RippleHelper
+    {
+        #region BorderBrush
+
+        public static readonly DependencyProperty BorderBrushProperty =
+            DependencyProperty.RegisterAttached("BorderBrush",
+                typeof(Brush),
+                typeof(RippleHelper),
+                new PropertyMetadata(Brushes.Transparent));
+
+        public static Brush GetBorderBrush(DependencyObject element) => (Brush)element.GetValue(BorderBrushProperty);
+
+        public static void SetBorderBrush(DependencyObject element, Brush value) => element.SetValue(BorderBrushProperty, value);
+
+        #endregion
+    }
+}
---
+++ src/Quan.ControlLibrary/Themes/Controls/QuanRippleLine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Quan.ControlLibrary
+{
+    [TemplateVisualState(GroupName = ActivationStatesGroupName, Name = ActiveStateName)]
+    [TemplateVisualState(GroupName = ActivationStatesGroupName, Name = InactiveStateName)]
+    public class QuanRippleLine : Control
+    {
+        #region Contants
+
+        public const string ActivationStatesGroupName = "ActivationStates";
+
+        public const string ActiveStateName = "Active";
+        public const string InactiveStateName = "Inactive";
+
+        #endregion
+
+        #region Dependency Properties
+
+
+
+        #endregion
+
+        #region Constructor
+
+        static QuanRippleLine()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(QuanRippleLine), new FrameworkPropertyMetadata(typeof(QuanRippleLine)));
+        }
+
+        #endregion
+
+    }
+}
